07cf8fdacefb2eff0f57986f49d13220b80c1272
canknbr <cankanbur1@gmail.com>
2023-05-30 10:30:33 +0300

    Initial Commit

---
+++ ECommerceUI/ECommerceUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceUIApp.swift
+//  ECommerceUI
+//
+//  Created by Can Kanbur on 30.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
